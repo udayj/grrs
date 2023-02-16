11dc33e007496d3d0fa1590872cd04718c90a8a0
root <jhunjhunwala.uday@gmail.com>
2023-02-16 12:18:54 +0000

    using clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(unused)]
 use clap::Parser;
+use std::fs::read_to_string;
 
 #[derive(Parser)]
 struct Cli {
@@ -7,11 +9,14 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
+    let content = read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+
+        if line.contains(&(args.pattern)) {
+            println!("{}", line);
+        }
+    }
 
 }
