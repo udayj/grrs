0056655bc157cf4b43be4095ad30ad806927a95e
root <jhunjhunwala.uday@gmail.com>
2023-02-17 12:43:19 +0000

    test matching, refactoring

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,33 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), CustomError> {
     let args = Cli::parse();
-    let content = read_to_string(&args.path).expect("could not read file");
+    let content = read_to_string(&args.path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}",&args.path.display(), err)))?;
+
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
 
     for line in content.lines() {
-
-        if line.contains(&(args.pattern)) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
         }
     }
+}
 
+#[test]
+fn find_a_match() {
+
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
